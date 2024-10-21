32ec611ce1d15f858425c3a32b6db1267f600361
Mushkrem <alexvsander@outlook.com>
2024-10-22 01:26:15 +0200

    general

--- sudoku.c
+++ sudoku.c
@@ -1,17 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _global {
     int points;
     int position;
 } _global;
 
+typedef struct _label {
+    unsigned char bit;
+    int weight;
+    char* text;
+} _label;
+
 typedef struct _menu {
-    int position;
-    char* labels[];
+    unsigned char position;
+    _label** labels;
 } _menu;
 
-int main(int argc, char* argv[]) {    
+
+int read_labels_to(_label** labels) {
+    FILE* stream;
+    char text[81], bit[5];
+    int i = 0;
+
+    errno_t error = fopen_s(&stream, "labels.txt", "r");
+    if (error)
+        return EXIT_FAILURE;
+    
+    while (fscanf_s(stream, "%80[^:]:%4s\n", text, (unsigned)_countof(text), bit, (unsigned)_countof(bit)) != EOF) {
+        labels[i] = (_label*)malloc(sizeof(_label));
+        text[80] = '\0';
+        if (labels[i] == NULL)
+            return EXIT_FAILURE;
+
+        labels[i]->bit = bit; // doesnt work. why.
+        labels[i]->weight = i;
+        labels[i]->text = (char*)malloc(81 * sizeof(char));
+        if (labels[i]->text == NULL)
+            return EXIT_FAILURE;
+        snprintf(labels[i]->text, 80, text, i);
+
+        i++;
+    }
+}
+
+int main(int argc, char* argv[]) {
     typedef struct _game {
         int points;
         _global* global_ref;
@@ -23,5 +57,18 @@ int main(int argc, char* argv[]) {
     global.position = 1;
     game.global_ref = &global;
 
-    printf("Hello world!\n %d, %s", game.global_ref->position, " :)");
+    _label label;
+    _menu menu;
+
+    menu.labels = (_label**)malloc(8 * sizeof(_label));
+
+    read_labels_to(menu.labels);
+
+    for (int i = 0; i < 4; i++) {
+        printf("Label %d: bit=%d, weight=%d, text=%s\n", i, menu.labels[i]->bit, menu.labels[i]->weight, menu.labels[i]->text);
+    }
+
+
+
+    return EXIT_SUCCESS;
 }
